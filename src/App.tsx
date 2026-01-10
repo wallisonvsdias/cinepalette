40fb1e42d3cee08f621ea45fd9390d9a88fc77ed
Wallison Dias <wallison.dias.711@ufrn.edu.br>
2026-01-10 11:10:11 -0300

    feat: start project using Vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-cine-dark">
+      <h1 className="text-4xl font-bold text-cine-gold">
+        CinePalette
+      </h1>
+    </div>
+  )
+}
+
+export default App
