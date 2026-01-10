040c1ba2d0fe6c072704bbb0cbf3697669ad1595
Wallison Dias <wallison.dias.711@ufrn.edu.br>
2026-01-10 13:53:01 -0300

    feat: list the movies and its user score

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import api from './services/api';
+import type { Movie } from './types/movie';
 
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await api.get('/movie/popular');
+        setMovies(response.data.results);
+        console.log('Dados recebidos:', response.data.results);
+      } catch (error) {
+        console.error('Erro ao buscar filmes:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
   return (
-    <div className="flex h-screen items-center justify-center bg-cine-dark">
-      <h1 className="text-4xl font-bold text-cine-gold">
+    <div className="bg-cine-dark flex min-h-screen flex-col items-center p-10 text-white">
+      <h1 className="text-cine-gold mb-8 text-4xl font-bold">
         CinePalette
       </h1>
+      <ul className="grid grid-cols-1 gap-4 md:grid-cols-3">
+        {movies.map((movie) => (
+          <li key={movie.id} className="bg-cine-gray rounded-lg p-4">
+            <strong>{movie.title}</strong>
+            <p className="text-sm text-gray-400">User score: {movie.vote_average}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,6 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   params: {
     api_key: import.meta.env.VITE_API_KEY,
-    language: 'pt-BR',
   },
 });
 
